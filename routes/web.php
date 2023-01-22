e46ccd7b9aaa8a7290283cd9f82691375d79f175
Vladimir <voffka.kv@gmail.com>
2023-01-22 10:47:10 +0600

    123

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/admin', \App\Http\Controllers\AdminController::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
